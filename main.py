49ba9527b967ff7f3fd940aea1e77bb0684e9b7e
DMaladyn <damian.maladyn@gmail.com>
2022-10-25 21:40:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,472 @@
+import random
+
+def oneRoll():
+    outcome1 = "champion"
+    roll1 = random.randint(1, 377)
+    roll1 -= caitlyn
+    if roll1 <= 0 :
+        outcome1 = "caitlyn"
+    else:
+        roll1 -= camille
+        if roll1 <= 0:
+            outcome1 = "camille"
+        else:
+            roll1 -= darius
+            if roll1 <= 0:
+                outcome1 = "darius"
+            else:
+                roll1 -= ezreal
+                if roll1 <= 0:
+                    outcome1 = "ezreal"
+                else:
+                    roll1 -= garen
+                    if roll1 <= 0:
+                        outcome1 = "garen"
+                    else:
+                        roll1 -= graves
+                        if roll1 <= 0:
+                            outcome1 = "graves"
+                        else:
+                            roll1 -= illaoi
+                            if roll1 <= 0:
+                                outcome1 = "illaoi"
+                            else:
+                                roll1 -= kassadin
+                                if roll1 <= 0:
+                                    outcome1 = "kassadin"
+                                else:
+                                    roll1 -= poppy
+                                    if roll1 <= 0:
+                                        outcome1 = "poppy"
+                                    else:
+                                        roll1 -= singed
+                                        if roll1 <= 0:
+                                            outcome1 = "singed"
+                                        else:
+                                            roll1 -= twistedfate
+                                            if roll1 <= 0:
+                                                outcome1 = "Twisted Fate"
+                                            else:
+                                                roll1 -= twitch
+                                                if roll1 <= 0:
+                                                    outcome1 = "Twitch"
+                                                else:
+                                                    roll1 -= ziggs
+                                                    if roll1 <= 0:
+                                                        outcome1 = "ziggs"
+                                                    else: outcome1 = "XD"
+    return outcome1
+
+def twoRoll():
+    outcome2 = "champion"
+    roll2 = random.randint(1, 286)
+    roll2 -= blitzcrank
+    if roll2 <= 0 :
+        outcome2 = "blitzcrank"
+    else:
+        roll2 -= katarina
+        if roll2 <= 0:
+            outcome2 = "katarina"
+        else:
+            roll2 -= kogmaw
+            if roll2 <= 0:
+                outcome2 = "kogmaw"
+            else:
+                roll2 -= lulu
+                if roll2 <= 0:
+                    outcome2 = "lulu"
+                else:
+                    roll2 -= quinn
+                    if roll2 <= 0:
+                        outcome2 = "quinn"
+                    else:
+                        roll2 -= swain
+                        if roll2 <= 0:
+                            outcome2 = "swain"
+                        else:
+                            roll2 -= talon
+                            if roll2 <= 0:
+                                outcome2 = "talon"
+                            else:
+                                roll2 -= tristana
+                                if roll2 <= 0:
+                                    outcome2 = "tristana"
+                                else:
+                                    roll2 -= trundle
+                                    if roll2 <= 0:
+                                        outcome2 = "trundle"
+                                    else:
+                                        roll2 -= vi
+                                        if roll2 <= 0:
+                                            outcome2 = "vi"
+                                        else:
+                                            roll2 -= warwick
+                                            if roll2 <= 0:
+                                                outcome2 = "Warwick"
+                                            else:
+                                                roll2 -= zilean
+                                                if roll2 <= 0:
+                                                    outcome2 = "zilean"
+                                                else:
+                                                    roll2 -= zyra
+                                                    if roll2 <= 0:
+                                                        outcome2 = "zyra"
+                                                    else: outcome2 = "XD"
+    return outcome2
+
+
+def threeRoll():
+    outcome3 = "champion"
+    roll3 = random.randint(1, 234)
+    roll3 -= chogath
+    if roll3 <= 0 :
+        outcome3 = "chogath"
+    else:
+        roll3 -= ekko
+        if roll3 <= 0:
+            outcome3 = "ekko"
+        else:
+            roll3 -= gangplank
+            if roll3 <= 0:
+                outcome3 = "gangplank"
+            else:
+                roll3 -= heimerdinger
+                if roll3 <= 0:
+                    outcome3 = "heimerdinger"
+                else:
+                    roll3 -= leona
+                    if roll3 <= 0:
+                        outcome3 = "leona"
+                    else:
+                        roll3 -= lissandra
+                        if roll3 <= 0:
+                            outcome3 = "lissandra"
+                        else:
+                            roll3 -= malzahar
+                            if roll3 <= 0:
+                                outcome3 = "malzahar"
+                            else:
+                                roll3 -= missfortune
+                                if roll3 <= 0:
+                                    outcome3 = "Miss Fortune"
+                                else:
+                                    roll3 -= samira
+                                    if roll3 <= 0:
+                                        outcome3 = "Samira"
+                                    else:
+                                        roll3 -= shaco
+                                        if roll3 <= 0:
+                                            outcome3 = "Shaco"
+                                        else:
+                                            roll3 -= taric
+                                            if roll3 <= 0:
+                                                outcome3 = "Taric"
+                                            else:
+                                                roll3 -= vex
+                                                if roll3 <= 0:
+                                                    outcome3 = "Vex"
+                                                else:
+                                                    roll3 -= zac
+                                                    if roll3 <= 0:
+                                                        outcome3 = "Zac"
+                                                    else: outcome3 = "XD"
+    return outcome3
+
+
+def fourRoll():
+    outcome4 = "champion"
+    roll4 = random.randint(1, 132)
+    roll4 -= braum
+    if roll4 <= 0 :
+        outcome4 = "Braum"
+    else:
+        roll4 -= mundo
+        if roll4 <= 0:
+            outcome4 = "Dr Mundo"
+        else:
+            roll4 -= fiora
+            if roll4 <= 0:
+                outcome4 = "Fiora"
+            else:
+                roll4 -= janna
+                if roll4 <= 0:
+                    outcome4 = "Janna"
+                else:
+                    roll4 -= jhin
+                    if roll4 <= 0:
+                        outcome4 = "Jhin"
+                    else:
+                        roll4 -= lux
+                        if roll4 <= 0:
+                            outcome4 = "Lux"
+                        else:
+                            roll4 -= orianna
+                            if roll4 <= 0:
+                                outcome4 = "Orianna"
+                            else:
+                                roll4 -= seraphine
+                                if roll4 <= 0:
+                                    outcome4 = "Seraphine"
+                                else:
+                                    roll4 -= sion
+                                    if roll4 <= 0:
+                                        outcome4 = "Sion"
+                                    else:
+                                        roll4 -= urgot
+                                        if roll4 <= 0:
+                                            outcome4 = "Urgot"
+                                        else:
+                                            roll4 -= yone
+                                            if roll4 <= 0:
+                                                outcome4 = "Yone"
+                                            else:
+                                                outcome4 = "XD"
+    return outcome4
+
+
+def fiveRoll():
+    outcome4 = "champion"
+    roll5 = random.randint(1, 80)
+    roll5 -= akali
+    if roll5 <= 0 :
+        outcome5 = "Akali"
+    else:
+        roll5 -= galio
+        if roll5 <= 0:
+            outcome5 = "Galio"
+        else:
+            roll5 -= jayce
+            if roll5 <= 0:
+                outcome5 = "Jayce"
+            else:
+                roll5 -= jinx
+                if roll5 <= 0:
+                    outcome5 = "Jinx"
+                else:
+                    roll5 -= kaisa
+                    if roll5 <= 0:
+                        outcome5 = "Kaisa"
+                    else:
+                        roll5 -= tahmkench
+                        if roll5 <= 0:
+                            outcome5 = "Tahm Kench"
+                        else:
+                            roll5 -= viktor
+                            if roll5 <= 0:
+                                outcome5 = "Viktor"
+                            else:
+                                roll5 -= yuumi
+                                if roll5 <= 0:
+                                    outcome5 = "Yuumi"
+                                else:
+                                    outcome5 = "XD"
+    return outcome5
+
+
+caitlyn = 29
+camille = 29
+darius = 29
+ezreal = 29
+garen = 29
+graves = 29
+illaoi = 29
+kassadin = 29
+poppy = 29
+singed = 29
+twistedfate = 29
+twitch = 29
+ziggs = 29
+
+
+blitzcrank = 22
+katarina = 22
+kogmaw = 22
+lulu = 22
+quinn = 22
+swain = 22
+talon = 22
+tristana = 22
+trundle = 22
+vi = 22
+warwick = 22
+zilean = 22
+zyra = 22
+
+
+chogath = 18
+ekko = 18
+gangplank = 18
+heimerdinger = 18
+leona = 18
+lissandra = 18
+malzahar = 18
+missfortune = 18
+samira = 18
+shaco = 18
+taric = 18
+vex = 18
+zac = 18
+
+
+braum = 12
+mundo = 12
+fiora = 12
+janna = 12
+jhin = 12
+lux = 12
+orianna = 12
+seraphine = 12
+sion = 12
+urgot = 12
+yone = 12
+
+
+akali = 10
+galio = 10
+jayce = 10
+jinx = 10
+kaisa = 10
+tahmkench = 10
+viktor = 10
+yuumi = 10
+
+x = 5
+
+i = 2
+
+outcome = [0,1,2,3,4]
+
+oneCost = 0
+twoCost = 0
+threeCost = 0
+fourCost = 0
+fiveCost = 0
+
+oneCosts = 0
+twoCosts = 0
+threeCosts = 0
+fourCosts = 0
+fiveCosts = 0
+
+errors = 0
+
+level = int(input("Choose level: "))
+print("Press enter to reroll!")
+
+if level == 1:
+    oneCost = 100
+    twoCost = 0
+    threeCost = 0
+    fourCost = 0
+    fiveCost = 0
+
+elif level == 2:
+    oneCost = 100
+    twoCost = 0
+    threeCost = 0
+    fourCost = 0
+    fiveCost = 0
+
+elif level == 3:
+    oneCost = 75
+    twoCost = 25
+    threeCost = 0
+    fourCost = 0
+    fiveCost = 0
+
+elif level == 4:
+    oneCost = 55
+    twoCost = 30
+    threeCost = 15
+    fourCost = 0
+    fiveCost = 0
+
+elif level == 5:
+    oneCost = 45
+    twoCost = 33
+    threeCost = 20
+    fourCost = 2
+    fiveCost = 0
+
+elif level == 6:
+    oneCost = 25
+    twoCost = 40
+    threeCost = 30
+    fourCost = 5
+    fiveCost = 0
+
+elif level == 7:
+    oneCost = 19
+    twoCost = 30
+    threeCost = 35
+    fourCost = 15
+    fiveCost = 1
+
+elif level == 8:
+    oneCost = 15
+    twoCost = 20
+    threeCost = 35
+    fourCost = 25
+    fiveCost = 5
+
+elif level == 9:
+    oneCost = 10
+    twoCost = 15
+    threeCost = 30
+    fourCost = 30
+    fiveCost = 15
+elif level == 10:
+    oneCost = 5
+    twoCost = 10
+    threeCost = 20
+    fourCost = 40
+    fiveCost = 25
+elif level == 11:
+    oneCost = 1
+    twoCost = 2
+    threeCost = 12
+    fourCost = 50
+    fiveCost = 35
+else:
+    print("Learn levels in tft :)")
+
+
+while i > 0:
+    for x in range(x):
+        chance = random.randint(1, 100)
+        chance -= oneCost
+        if chance <= 0:
+            outcome[x] = oneRoll()  # roll one cost
+        else:
+            chance -= twoCost
+            if chance <= 0:
+                outcome[x] = twoRoll()  # roll two cost
+            else:
+                chance -= threeCost
+                if chance <= 0:
+                    outcome[x] = threeRoll()
+                else:
+                    chance -= fourCost
+                    if chance <= 0:
+                        outcome[x] = fourRoll()
+                    else:
+                        chance -= fiveCost
+                        if chance <= 0:
+                            outcome[x] = fiveRoll()
+                        else:
+                            errors += 1
+    print("1." + outcome[0] + " 2." + outcome[1] + " 3." + outcome[2] + " 4." + outcome[3] + " 5." + outcome[4])
+    x = 5
+    input()
+
+
+
+
+
+
+
+
+
+
+
+
